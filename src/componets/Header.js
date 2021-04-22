646e89746097c5a34875a4dfbd6e8337e795c794
Patrick Tuszakowski <mrtall500@gmail.com>
2021-04-22 17:21:07 -0400

    done

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1>Welcome to No Secrets!</h1>
+            <h1 className='center'>No Secrets</h1>
             {/* logo needs doing, styling the header */}
         </div>
     )
